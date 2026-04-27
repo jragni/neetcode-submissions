915cb27ea1a742c2e9b58dcd5e1f11bd3d49bafd
Jhensen Agni <37388027+jragni@users.noreply.github.com>
2026-04-27 11:59:34 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        self.stack = []
+
+        actions_dict = {
+            "C": self.stack.pop,
+            "D": self.double,
+            "+": self.sums,
+        }
+
+        for op in operations:
+            if op in actions_dict:
+                actions_dict[op]()
+            else:
+                self.stack.append(int(op))
+        
+        total = sum(*[self.stack])
+
+        return total
+    
+    def double(self):
+        self.stack.append(2*self.stack[-1])
+    
+    def sums(self):
+        self.stack.append(self.stack[-2] + self.stack[-1])
+            
+            
