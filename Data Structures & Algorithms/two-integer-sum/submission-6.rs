2bac96a3e046ca88bc6412798b81ea29416f3ed9
Jhensen Agni <37388027+jragni@users.noreply.github.com>
2026-06-12 01:12:43 -0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut seen = HashMap::new();
+        
+        for (i, &n) in nums.iter().enumerate() {
+            let potential = target - n;
+            if let Some(&j) = seen.get(&potential) {
+                return vec![j as i32, i as i32];
+            }
+            seen.insert(n, i);
+        }
+        vec![]
+    }
+}
